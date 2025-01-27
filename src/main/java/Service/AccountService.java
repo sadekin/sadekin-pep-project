294e73791e3dbd3a14484676e9c1277d24285bbc
sadekin k <sadekin@berkeley.edu>
2025-01-27 19:18:11 +0000

    created service classes

---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,18 @@
+package Service;
+
+import Model.Account; 
+import DAO.AccountDAO;
+
+import java.util.List;
+
+public class AccountService {
+    AccountDAO accountDAO; 
+
+    public AccountService() {
+        accountDAO = new AccountDAO(); 
+    }
+
+    public Account addAccount(Account account) {
+        return accountDAO.addAccount(account); 
+    }
+}
